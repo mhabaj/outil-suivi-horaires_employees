b2127091e9d27d9ee54a1044c99486ce7a5877b3
ALHABAJ Mahmod <mhabaj99@hotmail.com>
2020-05-20 19:32:41 +0200

    lala

--- Projet/AppliSuiviPointage/src/centralApplication/Company.java
+++ Projet/AppliSuiviPointage/src/centralApplication/Company.java
@@ -1,12 +1,19 @@
 package centralApplication;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Company {
+public class Company implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6607331681733486180L;
 
 	private int id_Company;
+
 	private String name_Company;
-	private ArrayList<Department> Department_List = new ArrayList<Department>();;
+	private ArrayList<Department> Department_List = new ArrayList<Department>();
 
 	/**
 	 * @param id_Company
@@ -86,10 +93,41 @@ public class Company {
 	public boolean isDepartmentValidName(String searched_Department_Name) {
 		for (Department department : Department_List) {
 			String current_Department_Name = department.getName_Department();
-			if (current_Department_Name == searched_Department_Name)
+			if (current_Department_Name.contentEquals(searched_Department_Name))
 				return true;
 		}
 		return false;
+	}
+
+	public void showEveryDepartmentName() {
+		for (Department department : Department_List) {
+			String current_Department_Name = department.getName_Department();
+			System.out.println(current_Department_Name);
+		}
+	}
+
+	public Department getDepartmentByName(String searched_Department_Name) throws Exception {
+		for (Department department : Department_List) {
+			String current_Department_Name = department.getName_Department();
+			if (current_Department_Name.equals(searched_Department_Name))
+				return department;
+		}
+		throw new Exception("Error Search: Department Not Found");
+	}
+	
+	public Department getDepartmentById(int searched_Department_Id) throws Exception {
+		for (Department department : Department_List) {
+			int current_Department_Id = department.getId_Department();
+			if (current_Department_Id == searched_Department_Id)
+				return department;
+		}
+		throw new Exception("Error Search: Department Not Found");
+	}
+
+	@Override
+	public String toString() { //A refaire avec string buffer
+		return "Company [id_Company=" + id_Company + ", name_Company=" + name_Company + ", Department_List="
+				+ Department_List + "]";
 	}
 
 }
